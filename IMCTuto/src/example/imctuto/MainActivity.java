2ea5a85e9a98469670aa704d62cd156129f0dbcc
Goontor <claverie_pierre@hotmail.fr>
2013-11-06 15:29:36 +0100

    dd

--- IMCTuto/src/example/imctuto/MainActivity.java
+++ IMCTuto/src/example/imctuto/MainActivity.java
@@ -11,12 +11,15 @@ import android.view.View.OnClickListener;
 import android.view.View.OnTouchListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
 	EditText weightText = null;
 	TextView resultText = null;
+	RadioButton metre = null;
+	RadioButton centimetre = null;
 	EditText sizeText = null;
 	Button calculateButton = null;
 	Button razButton = null;
@@ -24,6 +27,8 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
        setContentView(R.layout.activity_main);
+       metre = (RadioButton) findViewById(R.id.radioButton1);
+       centimetre = (RadioButton) findViewById(R.id.radioButton2);
        weightText = (EditText) findViewById(R.id.editText1);
        resultText = (TextView) findViewById(R.id.textView4);
        sizeText  = (EditText) findViewById(R.id.editText2);
@@ -32,22 +37,34 @@ public class MainActivity extends Activity {
         
         
         calculateButton.setOnClickListener(compute);
+        razButton.setOnClickListener(raz);
         
         
     }
 
 
-    public void raz(){
+    private OnClickListener raz = new View.OnClickListener() {
+    	public void onClick(View v) {
+    		String reboot = (String) getResources().getString(R.string.resultStandard);
+    		weightText.setText("");
+    		sizeText.setText("");
+    		resultText.setText(reboot);
+    	}
     	
-    }
+    };
     private OnClickListener compute = new View.OnClickListener() {
 
 		@Override
 		public void onClick(View v) {
-			
+			float resultFloat = 0;
 	        float weight = Float.valueOf(weightText.getText().toString());
 	        float size = Float.valueOf(sizeText.getText().toString());
-			float resultFloat=(weight/(size*size));
+	        if(metre.isChecked()){
+				resultFloat=(weight/(size*size));
+	        }
+	        else{
+	        	resultFloat = (weight/((size/100)*(size/100)));
+	        }
 			resultText.setText(String.valueOf(resultFloat));
 			
 		}
